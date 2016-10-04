e811a8efd5ba13cacb63ef618ab783dabedb5084
Future System User <future@10.31.250.235>
2016-10-04 14:53:48 +0900

    initial

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from redis import Redis
+import os
+import socket
+
+
+app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
+host = socket.gethostname()
+        
+@app.route('/')
+def hello():
+    redis.incr('hits')
+    return '\n Test2: Hello World!\n I have been seen %s times.\nMy Host name is %s\n\n' % (redis.get('hits') ,host)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=80, debug=True)
